ab824b55cdb4a752c242aeb9c5c6f4ed455e80af
Utkarsh Ojha <70041876+utkarsh-ojha@users.noreply.github.com>
2024-03-11 22:39:49 +0530

    Min Absolute diff

---
+++ src/Sorting/MinAbsoluteDiff.java
@@ -0,0 +1,19 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class MinAbsoluteDiff {
+    public static void main(String[] args) {
+        int[] input = {5, 7, 8, 3, 6};
+        System.out.println(minAbsDiff(input));
+    }
+
+    static int minAbsDiff(int[] arr) {
+        Arrays.sort(arr);
+        int min = Math.abs(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            min = Math.min(min, Math.abs(arr[i] - arr[i - 1]));
+        }
+        return min;
+    }
+}
